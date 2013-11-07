53e2453ccc30b529318bb5e966c990395bcd765c
Matt Coneybeare <coneybeare@gmail.com>
2013-11-07 07:48:28 -0500

    Update to 0.1.7

--- UAAppReviewManagerExample/Pods/Pods-UAAppReviewManagerExampleiOS-environment.h
+++ UAAppReviewManagerExample/Pods/Pods-UAAppReviewManagerExampleiOS-environment.h
@@ -10,5 +10,5 @@
 #define COCOAPODS_POD_AVAILABLE_UAAppReviewManager
 #define COCOAPODS_VERSION_MAJOR_UAAppReviewManager 0
 #define COCOAPODS_VERSION_MINOR_UAAppReviewManager 1
-#define COCOAPODS_VERSION_PATCH_UAAppReviewManager 1
+#define COCOAPODS_VERSION_PATCH_UAAppReviewManager 7
 
--- UAAppReviewManagerExample/Pods/Pods-UAAppReviewManagerExampleiOS-resources.sh
+++ UAAppReviewManagerExample/Pods/Pods-UAAppReviewManagerExampleiOS-resources.sh
@@ -29,6 +29,8 @@ install_resource()
       echo "xcrun momc \"${PODS_ROOT}/$1\" \"${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcdatamodeld`.momd\""
       xcrun momc "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcdatamodeld`.momd"
       ;;
+    *.xcassets)
+      ;;
     /*)
       echo "$1"
       echo "$1" >> "$RESOURCES_TO_COPY"
@@ -39,7 +41,16 @@ install_resource()
       ;;
   esac
 }
-install_resource "${TARGET_BUILD_DIR}/UAAppReviewManager-iOS.bundle"
+install_resource "${BUILT_PRODUCTS_DIR}/UAAppReviewManager-iOS.bundle"
 
-rsync -avr --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+if [[ "${ACTION}" == "install" ]]; then
+  rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${INSTALL_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+fi
 rm -f "$RESOURCES_TO_COPY"
+
+if [ `find . -name '*.xcassets' | wc -l` -ne 0 ]
+then
+  DEVICE=`if [ "${TARGETED_DEVICE_FAMILY}" -eq 1 ]; then echo "iphone"; else echo "ipad"; fi`
+  find "${PWD}" -name "*.xcassets" -print0 | xargs -0 actool --output-format human-readable-text --notices --warnings --platform "${PLATFORM_NAME}" --minimum-deployment-target "${IPHONEOS_DEPLOYMENT_TARGET}" --target-device "${DEVICE}" --compress-pngs --compile "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.${WRAPPER_EXTENSION}"
+fi
